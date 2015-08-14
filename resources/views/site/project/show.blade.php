80717fae0d7d4bf7cd16b055768f9e699a4f0d1f
StevenmHernandez <contact@stevenHdesigns.com>
2015-08-14 01:37:11 +0000

    changed style of main image/videos

--- resources/views/site/project/show.blade.php
+++ resources/views/site/project/show.blade.php
@@ -1,9 +1,13 @@
 @extends('site.project.layout')
 
 @section('content')
-{!!$project->image!!}
-<h2>{{$project->title}} <small class="accent">{{$project->date}}</small></h2>
-<p class="bold">{{$project->subtitle}}</p>
-{!!$project->summary!!}
-{!!$project->body!!}
+    <div class="mainImage">
+        {!!$project->image!!}
+    </div>
+    <h2>{{$project->title}}
+        <small class="accent">{{$project->date}}</small>
+    </h2>
+    <p class="bold">{{$project->subtitle}}</p>
+    {!!$project->summary!!}
+    {!!$project->body!!}
 @stop
