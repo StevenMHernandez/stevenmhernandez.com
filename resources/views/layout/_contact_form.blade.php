b2e46782bab4b4c853a949407566b47daa988ac9
StevenmHernandez <contact@stevenhdesigns.com>
2016-10-01 23:31:32 -0400

    re-added Raleway, contact form is now an aside

--- resources/views/layout/_contact_form.blade.php
+++ resources/views/layout/_contact_form.blade.php
@@ -1,4 +1,4 @@
-<section class="contact" id="contact">
+<aside class="contact" id="contact">
     <p class="contact_header">Contact. <span class="accent">Steven Hernandez</span></p>
     @if($errors->has())
         @foreach ($errors->all() as $error)
@@ -25,4 +25,4 @@
     <p>Call me at : <a href="tel:{{stripNonNumeric(env('CONTACT_PHONE'))}}">{{env('CONTACT_PHONE')}}</a></p>
 
     <p>or reach me at : <a href="mailto:{{env('CONTACT_EMAIL')}}">{{env('CONTACT_EMAIL')}}</a></p>
-</section>
+</aside>
