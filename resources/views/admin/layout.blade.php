5c85116cc09128358371370241f599a72ed96859
StevenMHernandez <contact@stevenhdesigns.com>
2017-12-04 11:06:38 -0500

    update font to Inconsolata

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -5,7 +5,7 @@
 
         <link href="/assets/css/style.css" rel="stylesheet" type="text/css"/>
         <link href="/assets/css/admin.css" rel="stylesheet" type="text/css"/>
-        <link href='http://fonts.googleapis.com/css?family=Raleway:900,500' rel='stylesheet' type='text/css'>
+        <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
         <link rel="shortcut icon" href="/assets/images/logo.png">
--- resources/views/layout/_head.blade.php
+++ resources/views/layout/_head.blade.php
@@ -3,7 +3,7 @@
 <link href="/assets/js/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
 <link rel="shortcut icon" href="/assets/images/logo.png">
 <link href="/assets/images/logo.png" rel="apple-touch-icon-precomposed"/>
-<link href='https://fonts.googleapis.com/css?family=Raleway:400,900' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
 <meta name="description" content="{{ !empty($meta_description) ? $meta_description : '' }}">
 <title>{{ !empty($title) ? $title : 'Graphic Design, Web Design and Web Development.' }} | {{ env('SITE_TITLE') }}</title>
